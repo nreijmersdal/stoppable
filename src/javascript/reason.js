da71d1fe326474c80a95e1fc2816d497db40b68a
Niels van Reijmersdal <niels@vanreijmersdal.nl>
2018-02-16 11:14:40 +0100

    Refactored the reason checker to make it more readable.

--- src/javascript/reason.js
+++ src/javascript/reason.js
@@ -1,32 +1,38 @@
 (function reason() {
   exports.isValid = (text) => {
-    let result = { valid: true, message: '' };
     const words = text.trim().split(' ');
-    const vowelCheck = allWordsContainVowels(words);
-    if (!vowelCheck.passed) result = { valid: false, message: `Missing vowels in: ${vowelCheck.words.join(', ')}` };
-    const recurCheck = containsRecurringPattern(words);
-    if (!recurCheck.passed) result = { valid: false, message: `Recurring pattern: ${recurCheck.pattern}` };
-    if (words.length < 4) result = { valid: false, message: `Still ${4 - words.length} word(s) left` };
-    if (text.length < 30) result = { valid: false, message: `Still ${30 - text.length} character(s) left` };
+    let result = { valid: true, message: '' };
+    result = checkForVowels(words) || result;
+    result = checkForRecurringPattern(words) || result;
+    result = checkNumberOfWords(words) || result;
+    result = checkReasonLength(text) || result;
 
     return result;
   };
 
-  function containsRecurringPattern(array) {
-    let passed = true;
+  function checkReasonLength(text) {
+    if (text.length < 30) return { valid: false, message: `Still ${30 - text.length} character(s) left` };
+    return undefined;
+  }
+
+  function checkNumberOfWords(words) {
+    if (words.length < 4) return { valid: false, message: `Still ${4 - words.length} word(s) left` };
+    return undefined;
+  }
+
+  function checkForRecurringPattern(array) {
     let pattern;
     array.forEach((word) => {
       const count = array.filter(value => value === word).length;
       if (count > 2) {
-        passed = false;
         pattern = word;
       }
     });
-    return { passed, pattern };
+    if (pattern) return { valid: false, message: `Recurring pattern: ${pattern}` };
+    return null;
   }
 
-  function allWordsContainVowels(array) {
-    let passed = true;
+  function checkForVowels(array) {
     const words = [];
     array.forEach((word) => {
       if (!word.toLowerCase().includes('a') &&
@@ -35,11 +41,9 @@
           !word.toLowerCase().includes('o') &&
           !word.toLowerCase().includes('u') &&
           !word.toLowerCase().includes('y')
-      ) {
-        passed = false;
-        words.push(word);
-      }
+      ) words.push(word);
     });
-    return { passed, words };
+    if (words.length > 0) return { valid: false, message: `Missing vowels in: ${words.join(', ')}` };
+    return null;
   }
 }());
